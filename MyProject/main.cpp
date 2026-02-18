623e052b0c2388d5bc22254581fe2b71b551d94e
Richkid-2003 <mensahobed215@gmail.com>
2026-02-18 13:17:57 +0000

    Add createSession function and session vectors

--- MyProject/main.cpp
+++ MyProject/main.cpp
@@ -33,7 +33,6 @@ private:
     int duration;
 
 public:
-    // Constructor
     AttendanceSession(string code, string dt, string time, int dur) {
         courseCode = code;
         date = dt;
@@ -41,7 +40,6 @@ public:
         duration = dur;
     }
 
-    // Display Session Details
     void displaySession() {
         cout << "Course: " << courseCode << " | Date: " << date 
              << " | Time: " << startTime << " | Duration: " << duration << " hrs\n";
@@ -50,6 +48,8 @@ public:
 
 // --- GLOBAL VARIABLES ---
 vector<Student> students;
+// NEW: Vector to store sessions
+vector<AttendanceSession> sessions; 
 
 // --- HELPER FUNCTIONS ---
 void registerStudent() {
@@ -74,6 +74,40 @@ void viewStudents() {
         for (int i = 0; i < students.size(); i++) {
             cout << i + 1 << ". ";
             students[i].display();
+        }
+    }
+}
+
+// NEW: Function to create a lecture session
+void createSession() {
+    string code, date, time;
+    int duration;
+
+    cout << "\n--- Create Lecture Session ---\n";
+    cout << "Enter Course Code (e.g., EEE227): ";
+    cin >> code;
+    cout << "Enter Date (YYYY-MM-DD): ";
+    cin >> date;
+    cout << "Enter Start Time (HH:MM): ";
+    cin >> time;
+    cout << "Enter Duration (in hours): ";
+    cin >> duration;
+
+    AttendanceSession newSession(code, date, time, duration);
+    sessions.push_back(newSession);
+
+    cout << "Session created successfully!\n";
+}
+
+// NEW: Function to view all sessions
+void viewSessions() {
+    cout << "\n--- Scheduled Sessions ---\n";
+    if (sessions.empty()) {
+        cout << "No sessions created yet.\n";
+    } else {
+        for (int i = 0; i < sessions.size(); i++) {
+            cout << i + 1 << ". ";
+            sessions[i].displaySession();
         }
     }
 }
